b4b0153f663d910fdbd9050089bd1d7156c40e4d
bigmeech <denachural@gmail.com>
2015-02-05 03:38:01 +0000

    Added Now logo, Splash screen Done. Upgraded target SDK to 21.

--- app/src/main/java/com/rack10/yuse/app/activites/SplashActivity.java
+++ app/src/main/java/com/rack10/yuse/app/activites/SplashActivity.java
@@ -1,29 +1,40 @@
-package com.rack10.yuse.app;
+package com.rack10.yuse.app.activites;
 
-import android.app.Activity;
-import android.graphics.Typeface;
+import android.app.*;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.app.Activity;
+import android.os.Handler;
 
-//import  com.google.zxing.
+import com.rack10.yuse.app.R;
 
+public class SplashActivity extends Activity {
 
-public class MainActivity extends Activity {
+    private final int SPLASH_DELAY = 1000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        }, SPLASH_DELAY);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.menu_splash, menu);
         return true;
     }
 
@@ -33,9 +44,12 @@ public class MainActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
+
         return super.onOptionsItemSelected(item);
     }
 }
